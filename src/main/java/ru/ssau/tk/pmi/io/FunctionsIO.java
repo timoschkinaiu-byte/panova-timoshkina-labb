9572216a0deef3bfc25cbcb4c64101661fb0ceb7
Timoshkina <timoschkina-iu@yandex.ru>
2025-10-07 01:17:28 +0400

    метод serialize

--- src/main/java/ru/ssau/tk/pmi/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/pmi/io/FunctionsIO.java
@@ -29,7 +29,11 @@ public final class FunctionsIO {
         }
         out.flush();
     }
-
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
+        objectOutputStream.writeObject(function);
+        objectOutputStream.flush();
+    }
 
     public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException{
         ObjectInputStream objectInputStream = new ObjectInputStream(stream);
