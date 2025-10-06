74ff78de836b9c5c30ec07ac829c93c448e8f375
Timoshkina <timoschkina-iu@yandex.ru>
2025-10-07 01:22:13 +0400

    readTabulatedFunction

--- src/main/java/ru/ssau/tk/pmi/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/pmi/io/FunctionsIO.java
@@ -2,7 +2,10 @@ package ru.ssau.tk.pmi.io;
 
 import ru.ssau.tk.pmi.functions.Point;
 import ru.ssau.tk.pmi.functions.TabulatedFunction;
-
+import ru.ssau.tk.pmi.functions.factory.TabulatedFunctionFactory;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 import java.io.*;
 
 public final class FunctionsIO {
@@ -18,7 +21,24 @@ public final class FunctionsIO {
         printWriter.flush();
 
     }
-
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        String line = reader.readLine();
+        int count = Integer.parseInt(line.trim());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            line = reader.readLine();
+            String[] parts = line.split(" ");
+            try {
+                xValues[i] = formatter.parse(parts[0]).doubleValue();
+                yValues[i] = formatter.parse(parts[1]).doubleValue();
+            } catch (ParseException e) {
+                throw new IOException();
+            }
+        }
+        return factory.create(xValues, yValues);
+    }
 
     public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException{
         DataOutputStream out = new DataOutputStream(outputStream);
