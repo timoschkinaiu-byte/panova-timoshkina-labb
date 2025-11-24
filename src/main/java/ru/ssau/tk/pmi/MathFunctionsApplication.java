21cd017873f68fb382f6af1532625d688daa5097
Panova <apanova02@gmail.com>
2025-11-25 02:58:14 +0400

    ньюман тесты запросов

---
+++ src/main/java/ru/ssau/tk/pmi/MathFunctionsApplication.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.pmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathFunctionsApplication.class, args);
+    }
+}
